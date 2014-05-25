9a6278bb83d5fb12393c6e2cad7e30e72f25eefd
j <j@mailb.org>
2014-05-25 16:10:38 +0200

    only open available books

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -18,13 +18,17 @@ oml.ui.viewer = function() {
     that.updateElement = function() {
         item = ui.item;
         if (item) {
-            $iframe = $iframe || Ox.Element('<iframe>').css({
-                width: '100%',
-                height: '100%',
-                border: 0
-            }).appendTo(that);
-            $iframe.attr({
-                src: '/' + item + '/reader/'
+            oml.api.get({id: item, keys: ['mediastate']}, function(result) {
+                if (result.data.mediastate == 'available') {
+                    $iframe = $iframe || Ox.Element('<iframe>').css({
+                        width: '100%',
+                        height: '100%',
+                        border: 0
+                    }).appendTo(that);
+                    $iframe.attr({
+                        src: '/' + item + '/reader/'
+                    });
+                }
             });
         }
         return that;
