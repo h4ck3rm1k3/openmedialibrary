7e86e6810199d8340d16e8e7cb641429ffc21464
j <j@mailb.org>
2015-11-18 13:36:24 +0100

    quit on ctrl-q

--- oml/gtkwebkit.py
+++ oml/gtkwebkit.py
@@ -5,7 +5,7 @@ import subprocess
 
 import gi
 gi.require_version('WebKit2', '4.0')
-from gi.repository import WebKit2, Gtk
+from gi.repository import WebKit2, Gtk, Gdk
 
 
 base = dirname(dirname(dirname(abspath(__file__))))
@@ -17,7 +17,12 @@ def drop_cb(wid, context, x, y, time):
     context.finish(True, False, time)
     return True
 
+def on_key_press_event(widget, event):
+    if event.state & Gdk.ModifierType.CONTROL_MASK and event.keyval == 113:
+        Gtk.main_quit()
+
 wnd = Gtk.Window()
+
 wnd.set_icon_from_file(icon)
 wnd.set_wmclass(title, title)
 wnd.set_title(title)
@@ -26,6 +31,7 @@ wnd.set_default_size(1366, 768)
 ctx = WebKit2.WebContext.get_default()
 web = WebKit2.WebView.new_with_context(ctx)
 wnd.connect("destroy", Gtk.main_quit)
+wnd.connect('key_press_event', on_key_press_event)
 wnd.add(web)
 wnd.show_all()
 
